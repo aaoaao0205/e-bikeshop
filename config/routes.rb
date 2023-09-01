cea25e93bb16bb21b845c69365c4a9162e915404
aaoaao0205 <aaoaao0205@gmail.com>
2023-09-01 21:42:52 +0900

    商品詳細画面の作成・条件分岐

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :show, :new, :create]
+  resources :items, only: [:index, :show, :new, :create] do
+    post :add_to_cart, on: :member
+  end
 end
-
 
 
