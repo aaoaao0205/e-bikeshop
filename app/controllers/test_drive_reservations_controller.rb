b5385c242f362967973fb71ea55847b14f5a5c36
aaoaao0205 <aaoaao0205@gmail.com>
2023-09-07 02:37:56 +0900

    DatePickerの導入

--- app/controllers/test_drive_reservations_controller.rb
+++ app/controllers/test_drive_reservations_controller.rb
@@ -1,26 +1,31 @@
 class TestDriveReservationsController < ApplicationController
+  before_action :load_item, only: [:new, :create]
+
   def new
-    if params[:item_id]
-      @item = Item.find(params[:item_id])
-      @test_drive_reservation = @item.test_drive_reservations.new
-      @available_products = Item.pluck(:item_name)
-    else
-      render :new
-    end
+    @items = Item.all 
+    @test_drive_reservation = TestDriveReservation.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    @test_drive_reservation = @item.test_drive_reservations.build(test_drive_reservation_params)
-    if @test_drive_reservation.save
-      flash[:success] = "試乗予約が完了しました。"
-      redirect_to root_path
+    if @item
+      @test_drive_reservation = @item.test_drive_reservations.build(test_drive_reservation_params)
+      if @test_drive_reservation.save
+        flash[:success] = "試乗予約が完了しました。"
+        redirect_to root_path
+      else
+        render :new
+      end
     else
-      render :new
+      flash[:error] = "商品が見つかりませんでした。"
+      redirect_to root_path
     end
   end
 
   private
+
+  def load_item
+    @item = params[:item_id] ? Item.find(params[:item_id]) : nil
+  end
 
   def test_drive_reservation_params
     params.require(:test_drive_reservation).permit(:name, :email, :phone, :desired_product, :desired_date, :desired_time)
